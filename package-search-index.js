6ef71c01545e6b6c492dfa53c4c9a15e4aab1f0d
Imcab <Imcab@users.noreply.github.com>
2026-03-03 22:00:22 +0000

    deploy: bde51024c27b671387bce41c2259a99287dbf741

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.miequipo.mifeature"},{"l":"com.miequipo.mifeature.generated"},{"l":"com.tu.equipo.generated"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.stzteam.features.marsprocessor"},{"l":"com.stzteam.features.marsprocessor.generated"}];updateSearchResults();
