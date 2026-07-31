7b9ed1bb196d0636b882174496ab83f7713bae24
Imcab <A01798415@tec.mx>
2026-07-31 01:00:54 -0600

    v1.0.6

--- src/main/java/com/stzteam/features/marsprocessor/generated/FeatureConstants.java
+++ src/main/java/com/stzteam/features/marsprocessor/generated/FeatureConstants.java
@@ -2,6 +2,6 @@ package com.stzteam.features.marsprocessor.generated;
 
 // AUTO-GENERATED FILE DO NOT EDIT
 public final class FeatureConstants {
-    public static final String FEATURE_VERSION = "1.0.5";
+    public static final String FEATURE_VERSION = "1.0.6";
     public static final String FEATURE_NAME = "MarsProcessor";
 }
